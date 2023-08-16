09e0b0ffa5247708b5409993fd008f83fd60332b
Mani Poorna <ndmprao2001@gmail.com>
2023-08-16 11:15:34 +0530

    Version 1.1

--- src/components/States5.js
+++ src/components/States5.js
@@ -10,12 +10,13 @@ const States5 = ()=>{
             <div className="parent">
                 <h1>Parent Component</h1>
                 {
-                    login && <div className="child">
-                            <p>Username : <input type="text"/></p>
-                            <p>password : <input type="password"/></p>
-                            <button onClick={()=>setLogin(false)} type="submit">Login</button>
-                        
-                    </div>
+                    login && <form> 
+                        <div className="child">
+                                <p>Username : <input type="text"/></p>
+                                <p>password : <input type="password"/></p>
+                                <button onClick={()=>setLogin(false)} type="submit">Login</button>
+                        </div>
+                    </form>
                 }
                 {
                     !login && <p>You are logged in!</p>
